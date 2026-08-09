a0ef7eca787be269eef6a28fad2c9606a6671c1d
SAINATH KATHIRVEL <K60050852@gmail.com>
2026-08-09 09:45:37 +0530

    Clean up comments in LegalAIApplication
    Removed unnecessary comments from the LegalAIApplication class.

--- src/main/java/com/legalai/LegalAIApplication.java
+++ src/main/java/com/legalai/LegalAIApplication.java
@@ -3,10 +3,7 @@ package com.legalai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * LegalAI-Endee RAG Engine
- * Main Spring Boot Application
- */
+
 @SpringBootApplication
 public class LegalAIApplication {
     
